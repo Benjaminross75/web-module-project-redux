230f31c863134d184eb28068868286db6e009719
Ben Ross <mrbenross@gmail.com>
2023-11-01 14:36:24 -0500

    appTitle-connected

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {connect} from 'react-redux';
+//import reducer from '../reducers/movieReducer';
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const {appTitle} = props;
     const displayFavorites = true;
-    
+
     return(<div className="table-title">
         <div className="row">
         <div className="col-sm-6">
@@ -18,5 +19,9 @@ const MovieHeader = (props) => {
         </div>
     </div>);
 }
-
-export default MovieHeader;
+const mapStateToProps = state =>{
+    return({
+        appTitle: state.appTitle
+    })
+}
+export default connect(mapStateToProps)(MovieHeader);
